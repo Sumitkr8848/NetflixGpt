f7b1e2e936ea1e2f406cb73c544e4f3446fddcdc
sumit8848 <codersumit335@gmail.com>
2024-01-07 14:08:30 +0530

    saving api calls

--- src/hooks/useMovieTrailer.js
+++ src/hooks/useMovieTrailer.js
@@ -1,4 +1,4 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { API_OPTIONS } from "../utils/constants";
 import { addTrailerVideo } from "../utils/moviesSlice";
 import { useEffect } from "react";
@@ -6,6 +6,7 @@ import { useEffect } from "react";
 
 const useMovieTrailer = (movieId) => {
     const dispatch = useDispatch();
+    const movieTrailer=useSelector((store)=>store.movies.movieTrailer);
     const getMovieVideo = async () => {
         const data = await fetch('https://api.themoviedb.org/3/movie/'+movieId+'/videos?language=en-US', API_OPTIONS);
         const json = await data.json();
@@ -16,7 +17,7 @@ const useMovieTrailer = (movieId) => {
 
     }
     useEffect(() => {
-        getMovieVideo();
+        !movieTrailer && getMovieVideo();
     }, []);
 }
 
--- src/hooks/useUpcomingMovies.js
+++ src/hooks/useUpcomingMovies.js
@@ -1,11 +1,11 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { addUpcomingMovies } from "../utils/moviesSlice";
 import { API_OPTIONS } from "../utils/constants";
 import { useEffect } from "react";
 
 const useUpcomingMovies=()=>{
     const dispatch=useDispatch();
-
+    const upcomingMovies=useSelector((store)=>store.movies.upcomingMovies);
   const getUpcomingMovies=async()=>{
     const data=await fetch('https://api.themoviedb.org/3/movie/upcoming?page=1', API_OPTIONS);
     const json=await data.json();
@@ -13,7 +13,7 @@ const useUpcomingMovies=()=>{
   };
 
   useEffect(()=>{
-    getUpcomingMovies();
+    !upcomingMovies && getUpcomingMovies();
   },[])
 }
 
